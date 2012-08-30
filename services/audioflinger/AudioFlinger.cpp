b57b5dab30adfdfd2ae871096372c5bfa08281ec
Justin <jokersax@gmail.com>
2012-08-29 20:59:20 -0500

    last oops hopefully

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -1061,7 +1061,6 @@ status_t AudioFlinger::setParameters(audio_io_handle_t ioHandle, const String8& 
         }
     }
 #endif
-    }
 
     // hold a strong ref on thread in case closeOutput() or closeInput() is called
     // and the thread is exited once the lock is released
